7a9e56264b8eee88c7830e720132ed96632dd347
Renato Mendes <rnt.mendes11@gmail.com>
2024-05-14 10:13:54 -0300

    Crontoller Investiments

--- Back/src/controller/UserController.js
+++ Back/src/controller/UserController.js
@@ -3,18 +3,18 @@ const User = require("../model/User");
 class UserController {
     static async register(req, res) {
         const { name, email, password, confirmPassword, cpf, birth } = req.body;
-        if (!name) return res.status(400).json({ message: "Nome é obrigatório" });
-        if (!email) return res.status(400).json({ message: "Email é obrigatório" });
-        if (!password) return res.status(400).json({ message: "Senha é obrigatório" });
-        if (password != confirmPassword) return res.status(400).json({ message: "As senhas não são iguais" });
-        if (!cpf) return res.status(400).json({ message: "CPF é obrigatório" });
-        if (!birth) return res.status(400).json({ message: "Data de nascimento é obrigatória" });
+        if (!name) return res.status(400).send({ message: "Nome é obrigatório" });
+        if (!email) return res.status(400).send({ message: "Email é obrigatório" });
+        if (!password) return res.status(400).send({ message: "Senha é obrigatório" });
+        if (password != confirmPassword) return res.status(400).send({ message: "As senhas não são iguais" });
+        if (!cpf) return res.status(400).send({ message: "CPF é obrigatório" });
+        if (!birth) return res.status(400).send({ message: "Data de nascimento é obrigatória" });
 
-        // const emailExist = await User.findOne({ email: email });
-        // if (emailExist) return res.status(422).json({ message: "Já existe conta nesse e-mail" });
+        const emailExist = await User.findOne({ email: email });
+        if (emailExist) return res.status(422).send({ message: "Já existe conta nesse e-mail" });
 
-        // const cpfExist = await User.findOne({ cpf: cpf });
-        // if (cpfExist) return res.status(422).json({ message: "Já existe conta nesse cpf" });
+        const cpfExist = await User.findOne({ cpf: cpf });
+        if (cpfExist) return res.status(422).send({ message: "Já existe conta nesse cpf" });
 
 
         const user = new User({
@@ -44,19 +44,19 @@ class UserController {
             const { email, password } = req.body;
 
             if (!email || !password)
-                return res.status(400).json({ menssage: "E-mail e senha são obrigatórios" });
+                return res.status(400).send({ menssage: "E-mail e senha são obrigatórios" });
 
             const user = await User.findOne({ email });
-            if (!user) return res.status(404).json({ message: "Email não existente" });
+            if (!user) return res.status(404).send({ message: "Email não existente" });
 
             const passwordUser = await User.findOne({ password })
-            if (!passwordUser) return res.status(401).json({ menssage: "Senhao não existente" })
+            if (!passwordUser) return res.status(401).send({ menssage: "Senhao não existente" })
 
-            res.status(200).json({ message: "Login bem-sucedido" });
+            res.status(200).send({ message: "Login bem-sucedido" });
         } catch (error) {
             return res
                 .status(500)
-                .json({ message: "Erro ao logar!", data: error.menssage });
+                .send({ message: "Erro ao logar!", data: error.menssage });
         }
     }
 }
--- Back/src/routes/Investiment.js
+++ Back/src/routes/Investiment.js
@@ -4,5 +4,9 @@ const route = express.Router();
 
 route
     .post("/create", InvestimentController.createInvestiment)
+    .get("/getall", InvestimentController.getAll)
+    .get("/getid/:id", InvestimentController.getID)
+    .put("/update/:id", InvestimentController.updateInvestment)
+    .delete("/delete/:id", InvestimentController.deleteInvestiment)
 
 module.exports = route;
