86a60fc804affb75b351748530ac6a8cee9bfafa
mateusSleite <mateusleite2709@gmail.com>
2024-05-15 22:13:03 -0300

    atts

--- Front/quack-investimentos/src/components/InfoInvestments/main.jsx
+++ Front/quack-investimentos/src/components/InfoInvestments/main.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import style from "./style.module.css";
 import { Container, Grid, Checkbox } from "@mui/material";
-import { green } from "@mui/material/colors";
-import { dark } from "@mui/material/styles/createPalette";
+
+const handlePreviousPage = (setCurrentPage) => {
+  setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+};
+
+const handleNextPage = (setCurrentPage, totalPages) => {
+  setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+};
 
 export const InfoInvestments = () => {
   const options = ["RECEBIMENTOS", "DESPESAS FIXAS", "DESPESAS VARIAVEIS"];
 
   const [selectedCategory, setSelectedCategory] = useState("RECEBIMENTOS");
-
-  const handleCategorySelect = (category) => {
-    setSelectedCategory(category);
-  };
-
-  const data = [
+  const [data, setData] = useState([
     {
+      id: 1,
       vencimento: "01/05/2024",
       descricao: "Aluguel",
       valor: 1000,
@@ -23,6 +25,7 @@ export const InfoInvestments = () => {
       pago: false,
     },
     {
+      id: 2,
       vencimento: "10/05/2024",
       descricao: "Internet",
       valor: 80,
@@ -31,6 +34,7 @@ export const InfoInvestments = () => {
       pago: true,
     },
     {
+      id: 3,
       vencimento: "15/05/2024",
       descricao: "Salário",
       valor: 2500,
@@ -39,6 +43,7 @@ export const InfoInvestments = () => {
       pago: false,
     },
     {
+      id: 4,
       vencimento: "20/05/2024",
       descricao: "Energia",
       valor: 150,
@@ -47,6 +52,7 @@ export const InfoInvestments = () => {
       pago: false,
     },
     {
+      id: 5,
       vencimento: "25/05/2024",
       descricao: "Manutenção",
       valor: 200,
@@ -54,7 +60,72 @@ export const InfoInvestments = () => {
       categoria: "Despesas Variaveis",
       pago: false,
     },
-  ];
+    {
+      id: 6,
+      vencimento: "25/05/2024",
+      descricao: "Manutenção",
+      valor: 200,
+      recebidoDe: "Oficina Y",
+      categoria: "Despesas Variaveis",
+      pago: false,
+    },
+    {
+      id: 7,
+      vencimento: "25/05/2024",
+      descricao: "Manutenção",
+      valor: 200,
+      recebidoDe: "Oficina Y",
+      categoria: "Despesas Variaveis",
+      pago: false,
+    },
+    {
+      id: 8,
+      vencimento: "25/05/2024",
+      descricao: "Manutenção",
+      valor: 200,
+      recebidoDe: "Oficina Y",
+      categoria: "Despesas Variaveis",
+      pago: false,
+    },
+    {
+      id: 9,
+      vencimento: "25/05/2024",
+      descricao: "Manutenção",
+      valor: 200,
+      recebidoDe: "Oficina Y",
+      categoria: "Despesas Variaveis",
+      pago: false,
+    },
+  ]);
+
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleCheckboxChange = (id) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((item) => item !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  };
+
+  const handleRemove = () => {
+    const newData = data.filter((item) => !selectedItems.includes(item.id));
+    setData(newData);
+    setSelectedItems([]);
+  };
+
+  const getCurrentPageData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return data.slice(startIndex, endIndex);
+  };
 
   return (
     <Container sx={{ mt: 2 }} style={{ paddingRight: 0 }}>
@@ -89,7 +160,9 @@ export const InfoInvestments = () => {
           <button className={style.button} style={{ marginLeft: "2em" }}>
             Adicionar
           </button>
-          <button className={style.button}>Remover</button>
+          <button className={style.button} onClick={handleRemove}>
+            Remover
+          </button>
         </Grid>
         <div style={{ display: "flex", padding: "1.2em 1em 0 1em" }}>
           <Grid item xs={0.5}></Grid>
@@ -121,17 +194,17 @@ export const InfoInvestments = () => {
         >
           <div className={style.border}>
             <div className={style.border2}>
-              {data.map((item, index) => (
+              {getCurrentPageData().map((item) => (
                 <Grid
                   sx={{
                     display: "flex",
                     alignItems: "center",
                     backgroundColor:
-                      index % 2 === 0 ? "rgba(22, 137, 144, 0.1)" : "ffffff",
+                      item.id % 2 === 0 ? "rgba(22, 137, 144, 0.1)" : "ffffff",
                   }}
                   item
                   xs={12}
-                  key={index}
+                  key={item.id}
                 >
                   <Grid item xs={0.5}>
                     <Checkbox
@@ -141,6 +214,7 @@ export const InfoInvestments = () => {
                           color: "rgba(22, 137, 144, 1)",
                         },
                       }}
+                      onChange={() => handleCheckboxChange(item.id)}
                     />
                   </Grid>
                   <Grid item xs={2}>
@@ -165,6 +239,37 @@ export const InfoInvestments = () => {
                   </Grid>
                 </Grid>
               ))}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "baseline",
+              justifyContent: "space-between",
+              padding: "0.3em 0 0 1.2em",
+            }}
+          >
+            <span
+              style={{ color: "rgba(0, 0, 0, 0.70)", fontWeight: "bold" }}
+            >
+              Mostrando de {(currentPage - 1) * itemsPerPage + 1} até{" "}
+              {Math.min(currentPage * itemsPerPage, data.length)} de{" "}
+              {data.length} Registros
+            </span>
+            <div>
+              <button
+                className={style.nextdown}
+                onClick={() => handlePreviousPage(setCurrentPage)}
+              >
+                Anterior
+              </button>
+              <span style={{ color: "black" }}>{currentPage}</span>
+              <button
+                className={style.nextdown}
+                onClick={() => handleNextPage(setCurrentPage, totalPages)}
+              >
+                Próximo
+              </button>
             </div>
           </div>
         </div>
